b3a5a5e46ac823107ced444b3116b72108218485
jed <jed@nas>
2019-08-13 20:21:48 -0400

    Fixed host for ssl gen

--- overlay/post_install/12_ssl.sh
+++ overlay/post_install/12_ssl.sh
@@ -1,6 +1,7 @@
-#
-HOST=`tail -n1 /etc/hosts | cut -f2`
+#!/bin/sh
 
+# Get the host.
+HOST=`sysrc -n hostname`
 # Generate SSL Certs
 /usr/bin/openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/selfsigned.key -out /etc/ssl/selfsigned.crt -subj "/C=GB/ST=/L=/O=/OU=/CN=${HOST}"
 /usr/bin/openssl dhparam -out /etc/ssl/dhparam.pem 1024
